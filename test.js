fc785e12e132f076ae96cc0240343681b182d325
arias3601 <3601arias@gmail.com>
2016-05-03 08:45:37 -0600

    Adding part 3 stuff

---
+++ test.js
@@ -0,0 +1,11 @@
+example = [
+  {"Name":"Asher",
+   "Age":"20"},
+  {"Name":"JT",
+   "Age":"30"},
+  {"Name":"Bat Man",
+   "Age":"???"},
+  {"Name":"Sam",
+   "Age":"8"},
+];
+document.getElementById("demo").innerHTML = example[2].Name + " is " + example[2].Age + " years old.";
